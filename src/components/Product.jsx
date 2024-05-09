d97890f3e648b9cc072429c3c9e072b65d6655a4
Velykobrat <149200949+Velykobrat@users.noreply.github.com>
2024-05-09 15:58:15 +0300

    props

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,12 @@
-export const Product = () => {
-	const price = 999;
-	const imgUrl = "<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>";
-
-  return (
-    <>
-      <h2>Tacos</h2>
-			<img src={imgUrl} alt="Tacos With Lime" width="640" />
-      <p>Price: {price} credits</p>
-    </>
-  );
-};
+export const Product = ({ name, imgUrl, price }) => {
+    return (
+      <div>
+        <h2>{name}</h2>
+              <img src={imgUrl} alt={name} width="480" />
+              <p>Price: {price} credits</p>
+      </div>
+    );
+  };
     
     export default Product;
 
