bb775c4b45dfa148c9f8bb5e3af0e6db82d783c6
Velykobrat <149200949+Velykobrat@users.noreply.github.com>
2024-05-09 17:17:37 +0300

    BookList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
-import './App.css'
-import { Product } from "./Product";
+// App.jsx
+import React from 'react';
+import BookList from './BookList';
+import Product from './Product';
+import './App.css';
 
-export default function App() {
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
+
+const App = () => {
   return (
     <div>
       <h1>Best selling</h1>
@@ -16,6 +25,11 @@ export default function App() {
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
       />
+
+      <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
     </div>
   );
-}
+};
+
+export default App;
---
+++ src/components/BookList.jsx
@@ -0,0 +1,14 @@
+// BookList.jsx
+import React from 'react';
+
+const BookList = ({ books }) => {
+  return (
+    <ul>
+      {books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      })}
+    </ul>
+  );
+};
+
+export default BookList;
