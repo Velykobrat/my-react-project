d43fdb5f152dbfc4e4cd1208d2fbd7c0d5fb03bc
Velykobrat <149200949+Velykobrat@users.noreply.github.com>
2024-05-10 13:44:15 +0300

    CSS-модулі

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,17 +1,15 @@
 // src/components/Alert.jsx
 
 import clsx from "clsx";
-import "./Alert.css";
+import css from "./Alert.module.css";
 
 export const Alert = ({ variant, outlined, elevated, children }) => {
   return (
     <p
-      className={clsx(
-        "alert",
-        variant,
-        outlined && "is-outlined",
-        elevated && "is-elevated"
-      )}
+      className={clsx(css[variant], {
+        [css.isOutlined]: outlined,
+        [css.isElevated]: elevated,
+      })}
     >
       {children}
     </p>
